7b4e360e7e98482c133f884e4db0c0d57a69cd72
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-08-05 12:32:49 +0530

    Created Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+import Home from '../../pages/Home/Home'
+import {FaAngleDoubleLeft,FaAngleDoubleRight} from 'react-icons/fa';
+import './Layout.css'
+
+const Layout = () => {
+    const [toggle,setToggle] = useState(false)
+    const handleToggle = ()=>{
+        setToggle(!toggle)
+    }
+  return (
+    <>
+        <div className='sidebar-section'>
+            <div className={toggle?'sidebar':'sidebar-toggle'}>
+                <div className='sidebar-toggle-icons'>
+                    <p>
+                        {
+                            toggle?(
+                                <FaAngleDoubleRight size={30} onClick={handleToggle}/>
+                            ):(
+                                <FaAngleDoubleLeft size={30} onClick={handleToggle}/>
+                            )
+                        }
+                    </p>
+                </div>
+            </div>
+            <div className='container'>
+                <Home/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Layout
